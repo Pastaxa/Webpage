a3fbc8aefb66cbc36014682a82dd5e96e27305b8
therealmp213 <therealmp213@gmail.com>
2022-05-16 15:08:59 +0200

    adding square function

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,23 @@
-<script>
 
-</script>
+function pole()
+{
+	var a = document.getElementById("bok").value;
+	a=parseFloat(a);
+	p=a*a
+	document.getElementById("wynik").innerHTML=("Pole wynosi " + p);
+	if (a <=0)
+	{
+		document.getElementById("wynik").innerHTML=("Błąd");
+	}
+}
+function obwod()
+{
+	var a = document.getElementById("bok").value;
+	a=parseFloat(a);
+	p=a+a+a+a
+	document.getElementById("wynik").innerHTML=("Obwód wynosi " + p);
+	if (a <=0)
+	{
+		document.getElementById("wynik").innerHTML=("Błąd");
+	}
+}
